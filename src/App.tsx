2b05c7fe144f403c8360befa2156dcf16118e986
Khalid Jameer <kjameer0@gmail.com>
2022-11-14 20:07:29 -0500

    beginning fetching process

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+import Drawer  from "@mui/material/Drawer";
+import LinearProgres from "@mui/material/LinearProgress";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import AddShoppingCartIcon from "@mui/material/Icon";
+import Badge from "@mui/material/Badge";
+import { Wrapper } from "./app.styles";
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number
+}
+
+const getProducts = async (): Promise<CartItemType> => await(await fetch("https.://fakestoreapi.com/products")).json()
+
+
 const App = () => {
   return (
     <div className="App">Start</div>
